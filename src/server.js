397ee2ad3bbcfc7e47360ce061ed8f498f5ca0d8
Mateus Rizzo <mateusrizzo13@gmail.com>
2021-01-25 13:42:58 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+mongoose.connect(
+    'mongodb+srv://admin:OCvCYBbdUCwo39Gp@vuttr.gjxmq.mongodb.net/vuttrdb?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.use(express.json());
+
+app.listen(3000, () => {
+	console.log('server running on port 3000!')
+})
