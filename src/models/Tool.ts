d0d2d246ed6488cff9bd1fff6e931eab0b4c34a4
Mateus Rizzo <mateusrizzo13@gmail.com>
2021-01-28 18:06:59 -0300

    Chore/Added user id to tool schema

--- src/models/Tool.ts
+++ src/models/Tool.ts
@@ -7,6 +7,10 @@ const ToolSchema = new Schema({
 	link: String,
 	description: String,
 	tags: [String],
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
 });
 
 export default model<any, IToolModel>('Tool', ToolSchema);
