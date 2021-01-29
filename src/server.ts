dab8604e26213153c4283490ed1b24f306a15846
Mateus Rizzo <mateusrizzo13@gmail.com>
2021-01-29 16:20:24 -0300

    Chore/Added cors to server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import routes from './routes/index';
 
@@ -10,6 +11,7 @@ mongoose.connect(
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
